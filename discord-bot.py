b270fd209e8dfa7f234bd7b0367b356e384dc5a3
Sahil Jain <sahiljain.work@outlook.com>
2022-02-14 22:51:11 +0530

    added new command

--- discord-bot.py
+++ discord-bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import yfinance as yf
 from dotenv import load_dotenv
 import os
+import pandas as pd
 
 load_dotenv('.env')
 # client = discord.Client()
@@ -25,21 +26,26 @@ async def on_ready():
 #     file = discord.File("businesstimes.xlsx")
 #     await ctx.send(file=file, content="Here is your excel file")
 
-@bot.command()
-async def price(ctx, symbol):
-    current_price = yf.Ticker(f"{symbol}.NS").info['regularMarketPrice']
-    if current_price:
-        await ctx.send(f"The price of {symbol} is {current_price}")
-    else:
-        await ctx.send(f"{symbol} not found")
-
 # @bot.command()
-# async def info(ctx, symbol):
+# async def price(ctx, symbol):
 #     current_price = yf.Ticker(f"{symbol}.NS").info['regularMarketPrice']
+#     print(current_price)
 #     if current_price:
 #         await ctx.send(f"The price of {symbol} is {current_price}")
 #     else:
 #         await ctx.send(f"{symbol} not found")
-print(os.getenv('TOKEN'))
+
+@bot.command()
+async def info(ctx, symbol):
+    info_json = yf.Ticker(f"{symbol}.NS").info
+    if info_json['regularMarketPrice']:
+        info_df = pd.json_normalize(info_json).T.reset_index()
+        info_df.columns = ['Info', 'Details']
+        info_df.to_excel(f"{symbol}_info.xlsx")
+        file = discord.File(f"{symbol}_info.xlsx")
+        await ctx.send(file=file, content="Here is your excel file")
+    else:
+        await ctx.send(f"{symbol} not found")
+
 bot.run(os.getenv("TOKEN"))
 
